c79d72736212dad7a420b9bf3c830684fa1ffcc9
Ankit Kumar <144920510+ankityadav0011@users.noreply.github.com>
2025-01-24 13:06:43 +0530

    Update constant.js

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,3 +1,3 @@
-export const REACT_APP_BACKEND_URL = "http://localhost:5009";
+export const REACT_APP_BACKEND_URL = "https://chatapphanumanji.onrender.com/";
 
 export const REACT_APP_CLOUDINARY_CLOUD_NAME = "df7cpd4pu";
